56ab7b65701613b39ffaf085c67fbe52a2563c71
zingZing1298 <maheshvarma691@gmail.com>
2024-05-06 21:18:20 -0400

    Rendering a root console with a stereotypical '@'

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tcod
 
 def main():
-    # print("Hellow World!")
+    # print("Hello World!")
     screen_widht = 80
     screen_height = 50
 
     tileset = tcod.tileset.load_tilesheet('./data/asset.png', 32,8,tcod.tileset.CHARMAP_TCOD)
+
+    player_start_x = screen_widht//2
+    player_start_y =screen_height//2
 
     with tcod.context.new_terminal(
         screen_widht, 
@@ -13,13 +16,14 @@ def main():
         tileset= tileset,
         title = "RogueLike",
         vsync = True,
+
         ) as context:
             root_console = tcod.Console(screen_widht, screen_height, order = "F")
 
 
             #Main game loop
             while True:
-                root_console.print(x=1,y=1, string = "@")
+                root_console.print(x=player_start_x,y=player_start_y, string = "@")
                 context.present(root_console)
 
                 for event in tcod.event.wait():
